2567f150f1f3171aa0cc1b3be80ad2c9a7e9098d
Neal Guarddin <guarddin29@gmail.com>
2026-02-20 21:06:24 +0700

    Third push

--- backend/src/test/java/id/ac/ui/cs/advprog/backend/BackendApplicationTests.java
+++ backend/src/test/java/id/ac/ui/cs/advprog/backend/BackendApplicationTests.java
@@ -1,7 +1,6 @@
 package id.ac.ui.cs.advprog.backend;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
