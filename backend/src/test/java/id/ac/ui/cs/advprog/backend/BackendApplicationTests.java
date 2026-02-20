84571e2976969ab8d8f9894e37f40b450d563412
Neal Guarddin <guarddin29@gmail.com>
2026-02-20 21:02:33 +0700

    Second push

--- backend/src/test/java/id/ac/ui/cs/advprog/backend/BackendApplicationTests.java
+++ backend/src/test/java/id/ac/ui/cs/advprog/backend/BackendApplicationTests.java
@@ -1,13 +1,20 @@
 package id.ac.ui.cs.advprog.backend;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class BackendApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        assertNotNull(applicationContext, "Spring application context should be created");
     }
-
 }
