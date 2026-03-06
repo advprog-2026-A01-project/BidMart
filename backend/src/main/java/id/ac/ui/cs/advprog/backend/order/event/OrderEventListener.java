26be24bb89b212068397d0536a45932c8a6a58c2
sahilaathia <athiasahila@gmail.com>
2026-03-06 20:09:05 +0700

    backend: create directory structure for order and notification modules

---
+++ backend/src/main/java/id/ac/ui/cs/advprog/backend/order/event/OrderEventListener.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.backend.order.event;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderEventListener {
+    // TODO
+
+    // @Scheduled
+    public void handleWinnerDetermined() {
+        // TODO
+    }
+}
---
+++ backend/src/main/java/id/ac/ui/cs/advprog/backend/order/service/OrderService.java
@@ -0,0 +1,8 @@
+package id.ac.ui.cs.advprog.backend.order.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+    // TODO
+}
