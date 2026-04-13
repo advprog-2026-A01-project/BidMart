ae04f1730d36ad65e6d5ce51ee2ac48307b405f7
Neal Guarddin <guarddin29@gmail.com>
2026-04-13 12:45:17 +0700

    github actions fixed. Ready to deploy

--- backend/src/main/java/id/ac/ui/cs/advprog/backend/security/PermissionEnricherFilter.java
+++ backend/src/main/java/id/ac/ui/cs/advprog/backend/security/PermissionEnricherFilter.java
@@ -21,6 +21,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+
 @Component
 public class PermissionEnricherFilter extends OncePerRequestFilter {
 
